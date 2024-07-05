1191f7fba61fa16b45c688d7df188ecfaf7693dc
joshthomas98 <joshua.thomas98@hotmail.co.uk>
2024-07-05 03:27:27 +0100

    5/7/24 - 1

--- src/pages/ArtistUserProfile.js
+++ src/pages/ArtistUserProfile.js
@@ -1,27 +1,18 @@
 import { useState, useEffect, useContext } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { Modal, Button } from "react-bootstrap";
-import { LoginContext } from "../App";
 import Calendar from "react-calendar";
 import { format } from "date-fns-tz";
+import { LoginContext } from "../App";
 
 const ArtistUserProfile = () => {
-  const { userId, setUserId, artistOrVenue, setArtistOrVenue } =
-    useContext(LoginContext);
-
+  const { userId, artistOrVenue } = useContext(LoginContext);
   const { profileId } = useParams();
-
   const navigate = useNavigate();
-
-  if (!userId || !artistOrVenue) {
-    navigate("/signin");
-  } else if (userId && artistOrVenue === "V") {
-    navigate("/restrictedpage");
-  }
 
   const SERVER_BASE_URL = "http://localhost:8000/";
 
-  const [artist, setArtist] = useState([]);
+  const [artist, setArtist] = useState(null);
   const [unavailabilities, setUnavailabilities] = useState([]);
   const [selectedDate, setSelectedDate] = useState(null);
   const [selectedUnavailability, setSelectedUnavailability] = useState(null);
@@ -35,17 +26,12 @@ const ArtistUserProfile = () => {
           throw new Error("Failed to fetch artist data");
         }
         const data = await response.json();
-        console.log(data);
-        setArtist([data]);
+        setArtist(data);
       } catch (error) {
         console.log(error);
       }
     };
 
-    fetchArtist();
-  }, [profileId]);
-
-  useEffect(() => {
     const fetchUnavailabilities = async () => {
       try {
         const response = await fetch(
@@ -61,6 +47,7 @@ const ArtistUserProfile = () => {
       }
     };
 
+    fetchArtist();
     fetchUnavailabilities();
   }, [profileId]);
 
@@ -68,7 +55,6 @@ const ArtistUserProfile = () => {
     setShowModal(!showModal);
   };
 
-  // Function to handle date selection
   const handleDateSelect = (date) => {
     const dateString = formatWithTimezone(date);
     const isDateUnavailable = unavailabilities.some(
@@ -80,7 +66,7 @@ const ArtistUserProfile = () => {
         (u) => u.date === dateString
       );
       setSelectedUnavailability(unavailability);
-      setShowModal(true); // Show the modal
+      setShowModal(true);
     } else {
       setSelectedUnavailability(null);
     }
@@ -88,121 +74,66 @@ const ArtistUserProfile = () => {
     setSelectedDate(date);
   };
 
-  // Function to format a date in the desired timezone
   const formatWithTimezone = (date) => {
     const tz = "Europe/London";
     return format(date, "yyyy-MM-dd", { timeZone: tz });
   };
 
-  return (
-    <>
-      <h1 className="text-light text-center">My Profile</h1>
+  const renderProfileDetails = () => {
+    if (!artist) {
+      return null;
+    }
 
-      <section className="artist-profile-output text-light">
-        {artist.map((artist) => (
-          <div className="row py-5 px-4" key={artist.id}>
-            <div className="col-lg-8 mx-auto">
-              <div className="bg-dark shadow rounded overflow-hidden border border-secondary">
-                <div className="px-4 pt-0 pb-4 cover">
-                  <div className="media align-items-end profile-head text-center py-5">
-                    <div className="profile mr-3">
-                      <img
-                        src={SERVER_BASE_URL + artist.image}
-                        alt="..."
-                        width={130}
-                        className="rounded mb-2 img-thumbnail"
-                      />
-                    </div>
+    return (
+      <div className="container mt-5">
+        <div className="row justify-content-center">
+          <div className="col-lg-8">
+            <div className="card shadow-sm">
+              <div className="card-body">
+                <div className="text-center mb-4">
+                  <img
+                    src={SERVER_BASE_URL + artist.image}
+                    alt="Profile"
+                    className="img-fluid rounded-circle mt-3"
+                    style={{ width: 150, height: 150, objectFit: "cover" }}
+                  />
+                  <h2 className="mt-3 text-light">{artist.artist_name}</h2>
+                  <p className="text-light mt-3 lead">{artist.genre}</p>
+                  <p className="text-light lead">{artist.county}</p>
+                </div>
 
-                    <div className="media-body text-white">
-                      <h4 className="mt-3">{artist.artist_name}</h4>
-
-                      <p>{artist.county}</p>
-
-                      <h6 className="py-3">{artist.genre}</h6>
-                    </div>
-
-                    {userId === profileId ? (
-                      <a
-                        href="/artistprofilesettings"
-                        className="btn btn-outline-dark btn-sm btn-block text-light border-secondary mt-3"
-                      >
-                        Edit profile
-                      </a>
-                    ) : null}
-
-                    <div className="pt-5">
-                      {userId === profileId ? (
+                <div className="row justify-content-center mb-5 pt-3 text-light">
+                  {userId === profileId && (
+                    <>
+                      <div className="col-auto">
                         <Button
-                          color="purple"
-                          pill
+                          variant="primary"
+                          onClick={() => navigate("/artistprofilesettings")}
+                        >
+                          Edit Profile
+                        </Button>
+                      </div>
+                      <div className="col-auto">
+                        <Button
+                          variant="primary"
                           onClick={() => navigate("/mybookings")}
                         >
                           My Bookings
                         </Button>
-                      ) : (
-                        ""
-                      )}
-                    </div>
-
-                    {userId && artistOrVenue === "V" ? (
-                      <div className="leave-venue-feedback-btn text-center pt-5">
-                        <Button
-                          href={`/venuewritereview?artistId=${artist.id}`}
-                        >
-                          Leave feedback
-                        </Button>
                       </div>
-                    ) : userId && artistOrVenue === "A" ? null : null}
-                  </div>
+                    </>
+                  )}
                 </div>
 
-                {userId === profileId ? (
-                  <div className="advertise-new-gig text-center pb-4">
-                    <Button href="/artistadvertisegig">
-                      Advertise a New Gig
-                    </Button>
-                  </div>
-                ) : null}
-
-                {userId === profileId ? (
-                  <div className="show-listed-gigs text-center pb-4">
-                    <a href="/mylistedgigs">View my listed gigs</a>
-                  </div>
-                ) : null}
-
-                <div className="bg-dark p-4 d-flex justify-content-end text-center">
-                  <ul className="list-inline mb-0">
-                    <li className="list-inline-item">
-                      <h5 className="font-weight-bold mb-0 d-block">215</h5>
-                      <small className="text-light">Photos</small>
-                    </li>
-
-                    <li className="list-inline-item">
-                      <h5 className="font-weight-bold mb-0 d-block">745</h5>
-                      <small className="text-light">Followers</small>
-                    </li>
-
-                    <li className="list-inline-item">
-                      <h5 className="font-weight-bold mb-0 d-block">340</h5>
-                      <small className="text-light">Following</small>
-                    </li>
-                  </ul>
+                <div className="text-center mb-4">
+                  <h4 className="text-light pb-3 pt-3">About</h4>
+                  <p className="text-light">{artist.bio}</p>
                 </div>
 
-                <div className="px-4 py-3 text-center">
-                  <h4 className="mb-3 mt-3">About</h4>
-                  <div className="p-4 rounded shadow-sm bg-dark">
-                    <p className="font-italic mb-0 p-3">{artist.bio}</p>
-                  </div>
-                </div>
-
-                <div className="py-3 px-4 text-center">
-                  <h2>Availability</h2>
-
-                  {/* Display the calendar */}
+                <div className="text-center mb-4">
+                  <h4>Availability</h4>
                   <Calendar
-                    className="pt-3"
+                    className="mt-3"
                     value={selectedDate}
                     onChange={handleDateSelect}
                     tileClassName={({ date }) => {
@@ -212,64 +143,47 @@ const ArtistUserProfile = () => {
                         : "available-date";
                     }}
                   />
-
-                  {artistOrVenue === "A" && userId === profileId ? (
-                    <div className="pt-4">
-                      <Button href={`/artisteditavailability/${profileId}`}>
-                        Edit my availability
-                      </Button>
-                    </div>
-                  ) : null}
+                  {artistOrVenue === "A" && userId === profileId && (
+                    <Button
+                      variant="outline-dark"
+                      className="mt-3"
+                      onClick={() =>
+                        navigate(`/artisteditavailability/${profileId}`)
+                      }
+                    >
+                      Edit Availability
+                    </Button>
+                  )}
                 </div>
 
-                {artistOrVenue === "A" && userId === profileId ? (
-                  <div className="px-4 pb-2 pt-2">
-                    {/* Display the pop-up for unavailable dates */}
-                    {selectedUnavailability && (
-                      <div className="unavailability-popup">
-                        <h3>Date: {selectedUnavailability.date}</h3>
-                        <p className="pt-2">
-                          Status: {selectedUnavailability.status}
-                        </p>
-                        <p>Reason: {selectedUnavailability.reason}</p>
-                      </div>
-                    )}
-                  </div>
-                ) : null}
-
-                <div className="py-4 px-4">
-                  <div className="d-flex align-items-center justify-content-between mb-3">
-                    <h5 className="mb-0">Recent photos</h5>
-                    <a href="#" className="btn btn-link text-light">
-                      Show all
-                    </a>
-                  </div>
+                <div className="text-center text-light mb-4">
+                  <h4 className="pb-4">Recent Photos</h4>
                   <div className="row">
-                    <div className="col-lg-6 mb-2 pr-lg-1">
+                    <div className="col-lg-6 mb-3">
                       <img
-                        src="https://images.unsplash.com/photo-1469594292607-7bd90f8d3ba4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
-                        alt=""
+                        src="https://images.unsplash.com/photo-1469594292607-7bd90f8d3ba4?ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
+                        alt="Photo 1"
                         className="img-fluid rounded shadow-sm"
                       />
                     </div>
-                    <div className="col-lg-6 mb-2 pl-lg-1">
+                    <div className="col-lg-6 mb-3">
                       <img
-                        src="https://images.unsplash.com/photo-1493571716545-b559a19edd14?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
-                        alt=""
+                        src="https://images.unsplash.com/photo-1493571716545-b559a19edd14?ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
+                        alt="Photo 2"
                         className="img-fluid rounded shadow-sm"
                       />
                     </div>
-                    <div className="col-lg-6 pr-lg-1 mb-2">
+                    <div className="col-lg-6 mb-3">
                       <img
-                        src="https://images.unsplash.com/photo-1453791052107-5c843da62d97?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
-                        alt=""
+                        src="https://images.unsplash.com/photo-1453791052107-5c843da62d97?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
+                        alt="Photo 3"
                         className="img-fluid rounded shadow-sm"
                       />
                     </div>
-                    <div className="col-lg-6 pl-lg-1">
+                    <div className="col-lg-6 mb-3">
                       <img
-                        src="https://images.unsplash.com/photo-1475724017904-b712052c192a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
-                        alt=""
+                        src="https://images.unsplash.com/photo-1475724017904-b712052c192a?ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
+                        alt="Photo 4"
                         className="img-fluid rounded shadow-sm"
                       />
                     </div>
@@ -278,8 +192,40 @@ const ArtistUserProfile = () => {
               </div>
             </div>
           </div>
-        ))}
-      </section>
+        </div>
+      </div>
+    );
+  };
+
+  const renderUnavailabilityModal = () => {
+    return (
+      <Modal show={showModal} onHide={toggleModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Unavailable Date</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {selectedUnavailability && (
+            <>
+              <p>Date: {selectedUnavailability.date}</p>
+              <p>Status: {selectedUnavailability.status}</p>
+              <p>Reason: {selectedUnavailability.reason}</p>
+            </>
+          )}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={toggleModal}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  };
+
+  return (
+    <>
+      <h1 className="text-center text-light mt-5 mb-4">My Profile</h1>
+      {renderProfileDetails()}
+      {renderUnavailabilityModal()}
     </>
   );
 };
