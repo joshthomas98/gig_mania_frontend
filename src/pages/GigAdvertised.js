4115e02d5600d2d0a15980ab232f28c44f0f1804
joshthomas98 <joshua.thomas98@hotmail.co.uk>
2024-07-14 03:05:03 +0100

    14/7/24 - 4

--- src/pages/GigAdvertised.js
+++ src/pages/GigAdvertised.js
@@ -1,27 +1,32 @@
 import React from "react";
 import SuccessAnimation from "../components/SuccessAnimation";
+import { Button } from "react-bootstrap";
 
 const GigAdvertised = () => {
   const storedUserType = localStorage.getItem("artistOrVenue");
-  const storedUserId = localStorage.getItem("userId");
 
   return (
     <div className="text-light text-center">
-      <h2 className="pb-5">You have successfully advertised your gig!</h2>
-
-      <a
-        className="pb-3"
-        href={
-          storedUserType === "A"
-            ? `/artistuserprofile/${storedUserId}`
-            : `/venueuserprofile/${storedUserId}`
-        }
-      >
-        Back to my profile
-      </a>
-
-      <div style={{ position: "relative", top: "-110px" }}>
+      <div className="pb-5" style={{ position: "relative" }}>
         <SuccessAnimation />
+      </div>
+
+      <h2 className="pb-4">You have successfully advertised your gig!</h2>
+
+      <p className="pb-3">
+        Need further assistance? Contact our{" "}
+        <a href="/contactus">support team.</a>
+      </p>
+
+      <div className="pt-3">
+        <Button
+          variant="primary"
+          href={
+            storedUserType === "A" ? "artistuserprofile" : "venueuserprofile"
+          }
+        >
+          Back to my profile
+        </Button>
       </div>
     </div>
   );
